fcecd5dad30355f2407ce125ef62a46ce0925181
Russell <russell.s.stewart@gmail.com>
2015-07-03 23:27:50 -0700

    Updates to examples/tensor.py following stricter Tensor API.

--- examples/tensor.py
+++ examples/tensor.py
@@ -2,19 +2,19 @@ import apollo
 import numpy as np
 t = apollo.Tensor()
 t.reshape((2,3))
-t.set_mem(2)
+t.set_values(2)
 v = apollo.Tensor()
-v.reshape((3,3))
-v.set_mem(5)
+v.reshape((2,3))
+v.set_values(5)
 v += t
-print v.mem
+print v.get_mem()
 v *= 3
-print v.mem
+print v.get_mem()
 v *= t
-print v.mem
+print v.get_mem()
 v.copy_from(t)
-print v.mem
-c = v.mem
-c[:] = np.reshape(np.arange(v.count()), v.shape())
-print v.mem
-v.mem = c
+print v.get_mem()
+c = v.get_mem()
+c[:] = np.reshape(np.arange(v.count()), v.shape)
+print v.get_mem()
+v.set_mem(c)
