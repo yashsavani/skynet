05423f56acb8a460a78f381409ccf2155e8dddbc
Russell <russell.s.stewart@gmail.com>
2015-07-05 12:06:09 -0700

    Linted exmples/simple.py.

--- examples/simple.py
+++ examples/simple.py
@@ -4,11 +4,13 @@ import numpy as np
 
 net = apollo.Net()
 for i in range(1000):
-    example = np.array(np.random.random()).reshape((1,1,1,1))
+    example = np.array(np.random.random()).reshape((1, 1, 1, 1))
     net.forward_layer(layers.NumpyData(name='data', data=example))
     net.forward_layer(layers.NumpyData(name='label', data=(example*3)))
-    net.forward_layer(layers.Convolution(name='conv', kernel_size=1, bottoms=['data'], num_output=1))
-    loss = net.forward_layer(layers.EuclideanLoss(name='loss', bottoms=['conv', 'label']))
+    net.forward_layer(layers.Convolution(name='conv', kernel_size=1,
+        bottoms=['data'], num_output=1))
+    loss = net.forward_layer(layers.EuclideanLoss(name='loss',
+        bottoms=['conv', 'label']))
     net.backward()
     net.update(lr=0.1)
     if i % 100 == 0:
