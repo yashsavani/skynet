ee11563fc97146a126b48dd97a082bf7ee070fb4
Yash Savani <ysavani@deep6.stanford.edu>
2015-07-24 14:29:54 -0700

    include memory data in pycaffe

--- python/caffe/_caffe.cpp
+++ python/caffe/_caffe.cpp
@@ -16,6 +16,7 @@
 
 #include "caffe/caffe.hpp"
 #include "caffe/python_layer.hpp"
+#include "caffe/layers/memory_data_layer.hpp"
 
 // Temporary solution for numpy < 1.7 versions: old macro, no promises.
 // You're strongly advised to upgrade to >= 1.7.
