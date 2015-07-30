640d032ea966433c2abbecea07efcefadf6d6727
Russell <russell.s.stewart@gmail.com>
2015-07-29 20:33:28 -0700

    Moved Architecture into legacy.py

--- apollo/legacy.py
+++ apollo/legacy.py
@@ -5,13 +5,56 @@ import numpy as np
 import logging
 import argparse
 
+import caffe_pb2
+from google.protobuf.text_format import Merge
+from apollo import Net
+import layers
+
+class Architecture(object):
+    """
+    Class used when loading Caffe-style prototxts for legacy.py
+    """
+    def __init__(self, phase='train'):
+        self.layers = []
+        self.phase = phase
+        self.phase_map = { 'train': caffe_pb2.TRAIN, 'test': caffe_pb2.TEST }
+    def load_from_proto(self, prototxt):
+        net = caffe_pb2.NetParameter()
+        with open(prototxt, 'r') as f:
+            Merge(f.read(), net)
+        def xor(a, b):
+            return (a and (not b)) or ((not a) and b)
+        assert xor(len(net.layer) > 0, len(net.layers) > 0), \
+            "Net cannot have both new and old layer types."
+        if net.layer:
+            net_layers = net.layer
+        else:
+            net_layers = net.layers
+        for layer in net_layers:
+            include_phase_list = map(lambda x: x.phase, layer.include)
+            if len(include_phase_list) > 0 and self.phase_map[self.phase] not in include_phase_list:
+                continue
+            new_layer = layers.Unknown({})
+            new_layer.p = layer
+            if len(new_layer.p.top) == 0:
+                if str(layer.type) != "Silence":
+                    # hack to handle Silence layer not having a top
+                    new_layer.p.top.append(new_layer.p.name)
+            self.layers.append(new_layer)
+        return net
+    def forward(self, net):
+        loss = 0
+        for x in self.layers:
+            loss += net.forward_layer(x)
+        return loss
+
 def run(hyper):
     net = apollo.Net()
-    arch = apollo.Architecture()
+    arch = Architecture()
     arch.load_from_proto(hyper['net_prototxt'])
 
     test_net = apollo.Net(phase='test')
-    test_arch = apollo.Architecture(phase='test')
+    test_arch = Architecture(phase='test')
     test_arch.load_from_proto(hyper['net_prototxt'])
 
     arch.forward(net)
@@ -44,7 +87,7 @@ def run(hyper):
         if i % hyper['test_interval'] == 0:
             test_performance(net, test_net)
             if 'accuracy' in test_net.tops:
-                logging.info('Accuracy: %.2f' % test_net.tops['accuracy'].data.flatten()[0])
+                logging.info('Accuracy: %.5f' % test_net.tops['accuracy'].data.flatten()[0])
         if i % hyper['snapshot_interval'] == 0 and i > 0:
             net.save('%s_%d.h5' % (hyper['snapshot_prefix'], i))
 
